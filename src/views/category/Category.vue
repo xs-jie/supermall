59c1d78195bc7acbb5fa0ed28e1e159d08c456c0
xs-jie <13592841305.163.com>
2021-09-14 00:53:46 +0800

    profile

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,83 +1,143 @@
 <template>
-  <div class="wrapper" ref="wrapper">
-    <ul class="content">
-      <li>{1}</li>
-      <li>{2}</li>
-      <li>{3}</li>
-      <li>{4}</li>
-      <li>{5}</li>
-      <li>{6}</li>
-      <li>{7}</li>
-      <li>{8}</li>
-      <li>{9}</li>
-      <li>{10}</li>
-      <li>{11}</li>
-      <li>{12}</li>
-      <li>{13}</li>
-      <li>{14}</li>
-      <li>{15}</li>
-      <li>{16}</li>
-      <li>{17}</li>
-      <li>{18}</li>
-      <li>{19}</li>
-      <li>{20}</li>
-      <li>{21}</li>
-      <li>{22}</li>
-      <li>{23}</li>
-      <li>{24}</li>
-      <li>{25}</li>
-      <li>{26}</li>
-      <li>{27}</li>
-      <li>{28}</li>
-      <li>{29}</li>
-      <li>{30}</li>
-      <li>{31}</li>
-      <li>{32}</li>
-      <li>{33}</li>
-      <li>{34}</li>
-      <li>{35}</li>
-      <li>{36}</li>
-      <li>{37}</li>
-      <li>{38}</li>
-      <li>{39}</li>
-      <li>{40}</li>
-      <li>{41}</li>
-      <li>{42}</li>
-      <li>{43}</li>
-      <li>{44}</li>
-      <li>{45}</li>
-      <li>{46}</li>
-      <li>{47}</li>
-      <li>{48}</li>
-      <li>{49}</li>
-      <li>{50}</li>
-    </ul>
+  <div id="category">
+    <nav-bar class="nav-bar"><div slot="center">商品分类</div></nav-bar>
+    <div class="content">
+      <tab-menu :categories="categories"
+                @selectItem="selectItem">
+      </tab-menu>
+
+      <scroll id="tab-content" :data="[categoryData]">
+        <div>
+          <tab-content-category :subcategories="showSubcategory"></tab-content-category>
+          <tab-control :titles="['综合', '新品', '销量']"
+                       @itemClick="tabClick"></tab-control>
+          <tab-content-detail :category-detail="showCategoryDetail"></tab-content-detail>
+        </div>
+      </scroll>
+    </div>
   </div>
 </template>
 
 <script>
-import BScroll from "better-scroll";
+  import NavBar from "@/components/common/navbar/NavBar";
 
-export default {
-  name: "Category",
-  data() {
-    return {
-      scroll,
+  import TabMenu from './childComps/TabMenu'
+  import TabControl from "@/components/content/tabControl/TabControl";
+  import Scroll from "@/components/common/scroll/Scroll";
+  import TabContentCategory from './childComps/TabContentCategory'
+  import TabContentDetail from './childComps/TabContentDetail'
+
+  import {getCategory, getSubcategory, getCategoryDetail} from "network/category";
+  import {POP, SELL, NEW} from "@/common/const";
+  import {tabControlMixin} from "@/common/mixin";
+
+  export default {
+		name: "Category",
+    components: {
+		  NavBar,
+      TabMenu,
+      TabControl,
+      Scroll,
+      TabContentCategory,
+      TabContentDetail
+    },
+    mixins: [tabControlMixin],
+    data() {
+		  return {
+		    categories: [],
+        categoryData: {
+        },
+        currentIndex: -1
+      }
+    },
+    created() {
+		  // 1.请求分类数据
+      this._getCategory()
+    },
+    computed: {
+		  showSubcategory() {
+		    if (this.currentIndex === -1) return {}
+        return this.categoryData[this.currentIndex].subcategories
+      },
+      showCategoryDetail() {
+		    if (this.currentIndex === -1) return []
+		    return this.categoryData[this.currentIndex].categoryDetail[this.currentType]
+      }
+    },
+    methods: {
+		  _getCategory() {
+		    getCategory().then(res => {
+		      // 1.获取分类数据
+		      this.categories = res.data.category.list
+          // 2.初始化每个类别的子数据
+          for (let i = 0; i < this.categories.length; i++) {
+            this.categoryData[i] = {
+              subcategories: {},
+              categoryDetail: {
+                'pop': [],
+                'new': [],
+                'sell': []
+              }
+            }
+          }
+          // 3.请求第一个分类的数据
+          this._getSubcategories(0)
+        })
+      },
+      _getSubcategories(index) {
+        this.currentIndex = index;
+		    const mailKey = this.categories[index].maitKey;
+        getSubcategory(mailKey).then(res => {
+          this.categoryData[index].subcategories = res.data
+          this.categoryData = {...this.categoryData}
+          this._getCategoryDetail(POP)
+          this._getCategoryDetail(SELL)
+          this._getCategoryDetail(NEW)
+        })
+      },
+      _getCategoryDetail(type) {
+		    // 1.获取请求的miniWallkey
+        const miniWallkey = this.categories[this.currentIndex].miniWallkey;
+        // 2.发送请求,传入miniWallkey和type
+		    getCategoryDetail(miniWallkey, type).then(res => {
+		      // 3.将获取的数据保存下来
+		      this.categoryData[this.currentIndex].categoryDetail[type] = res
+          this.categoryData = {...this.categoryData}
+        })
+      },
+      /**
+       * 事件响应相关的方法
+       */
+      selectItem(index) {
+        this._getSubcategories(index)
+      }
     }
-  },
-  mounted() {
-    this.scroll = new BScroll(this.$refs.wrapper,{
-
-
-    })
-  }
-}
+	}
 </script>
 
 <style scoped>
-  .wrapper{
-    height: 300px;
-    background-color: #2c3e50;
-    /*overflow: hidden;*/
+  #category {
+    height: 100vh;
+  }
+
+  .nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
+
+  .content {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 44px;
+    bottom: 49px;
+
+    display: flex;
+  }
+
+  #tab-content {
+    height: 100%;
+    flex: 1;
   }
 </style>
